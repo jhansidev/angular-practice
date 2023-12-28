ebd9a65dc0f69951bbaec3c34d71e400008ec091
jhansi <jhansirani111@gmail.com>
2023-12-27 23:44:54 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, FormsModule } from '@angular/forms';
+import {error} from "@angular/compiler-cli/src/transformers/util";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, FormsModule, FormControl, FormBuilder],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngPractice';
+  myForm: FormGroup; // Declare the form as a class property
 
-  form = new FormGroup({
-    "firstName": new FormControl("", Validators.required),
-    "lastName": new FormControl("", Validators.required),
-    "email": new FormControl("", Validators.required),
-    "password": new FormControl("", Validators.required),
-    "address": new FormControl("", Validators.required),
-    "message": new FormControl(""),
-  });
-
-  onSubmit() {
-    console.warn("Your details has been submitted", this.form.value);
-    this.form.reset();
-    console.log(this.form);
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      "firstName": ["", Validators.required],
+      "lastName": ["", Validators.required],
+      "email": ["", Validators.required],
+      "password": ["", Validators.required],
+      "address": ["", Validators.required],
+      "message": [""]
+    });
   }
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {}
-
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.warn("Your details have been submitted", this.myForm.value);
+      this.myForm.reset();
+    } else {
+      console.log('error came here!!')
+    }
+  }
 }
