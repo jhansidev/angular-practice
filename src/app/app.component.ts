53dccc8ec792eef01d2e8ee94ce61f21afde0c50
jhansi <jhansirani111@gmail.com>
2024-01-04 16:40:19 -0500

    API call to get the list of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {ReactiveFormsModule, FormGroup, FormControl, Validators, FormBuilder, FormsModule } from '@angular/forms';
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import {ListComponent} from "./list/list.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, FormsModule, FormControl, FormBuilder],
+  imports: [CommonModule, RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ngPractice';
-  myForm: FormGroup; // Declare the form as a class property
 
-  constructor(private formBuilder: FormBuilder) {
-    this.myForm = this.formBuilder.group({
-      "firstName": ["", Validators.required],
-      "lastName": ["", Validators.required],
-      "email": ["", Validators.required],
-      "password": ["", Validators.required],
-      "address": ["", Validators.required],
-      "message": [""]
-    });
-  }
-
-  onSubmit() {
-    if (this.myForm.valid) {
-      console.warn("Your details have been submitted", this.myForm.value);
-      this.myForm.reset();
-    } else {
-      console.log('error came here!!')
-    }
-  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
 
+
+export class ListComponent implements OnInit{
+ httpClient = inject(HttpClient);
+  list: ProductListResponse = { products: [], total: 0, skip: 0, limit: 0 };
+ ngOnInit() {
+   this.fetchList();
+ }
+
+ fetchList(){
+   this.httpClient.get<ProductListResponse>('https://dummyjson.com/products').subscribe((response) => {
+     console.log(response);
+     this.list.products = response.products;
+   });
+ }
+
+}
+export interface ProductListResponse {
+  products: any[];
+  total: number;
+  skip: number;
+  limit: number;
 }
